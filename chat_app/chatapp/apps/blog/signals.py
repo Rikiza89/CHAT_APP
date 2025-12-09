1cee02cf617b3863e6504ed3dbcdf229bd3420ae
Nakata Riccardo <149764397+Rikiza89@users.noreply.github.com>
2025-12-10 08:19:22 +0900

    Add files via upload

--- chat_app/chatapp/apps/blog/signals.py
+++ chat_app/chatapp/apps/blog/signals.py
@@ -1,7 +1,7 @@
-# apps/blog/signals.py
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Post
+from .models import Post, Comment, PostLike
+from apps.accounts.models import Notification
 from django.utils.text import slugify
 import uuid
 
@@ -15,3 +15,33 @@ def generate_slug(sender, instance, **kwargs):
         else:
             base_slug = str(uuid.uuid4())[:8]
         instance.slug = f"{base_slug}-{str(uuid.uuid4())[:8]}"
+
+
+@receiver(post_save, sender=Comment)
+def notify_comment(sender, instance, created, **kwargs):
+    """Notify post author about new comment"""
+    if created and instance.author != instance.post.author:
+        # Check if user wants comment notifications
+        if hasattr(instance.post.author, 'profile') and instance.post.author.profile.notify_comments:
+            Notification.objects.create(
+                recipient=instance.post.author,
+                sender=instance.author,
+                notification_type='comment',
+                message=f'{instance.author.username} commented on your post',
+                link=f'/post/{instance.post.slug}/'
+            )
+
+
+@receiver(post_save, sender=PostLike)
+def notify_like(sender, instance, created, **kwargs):
+    """Notify post author about new like"""
+    if created and instance.user != instance.post.author:
+        # Check if user wants like notifications
+        if hasattr(instance.post.author, 'profile') and instance.post.author.profile.notify_likes:
+            Notification.objects.create(
+                recipient=instance.post.author,
+                sender=instance.user,
+                notification_type='like',
+                message=f'{instance.user.username} liked your post',
+                link=f'/post/{instance.post.slug}/'
+            )
--- chat_app/chatapp/apps/chat/signals.py
+++ chat_app/chatapp/apps/chat/signals.py
@@ -1,17 +1,21 @@
-# apps/chat/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Message
-from .tasks import send_message_notification, update_message_status
+from apps.accounts.models import Notification
 
 
 @receiver(post_save, sender=Message)
 def handle_new_message(sender, instance, created, **kwargs):
-    """Handle new message - send notifications"""
+    """Create notification for new message"""
     if created:
-        pass
-        # Send notification asynchronously
-        # send_message_notification.delay(str(instance.id))
-        
-        # # Update status to delivered
-        # update_message_status.delay(str(instance.id), 'delivered')
+        room_members = instance.room.members.exclude(id=instance.sender.id)
+        for member in room_members:
+            # Check if user wants message notifications
+            if hasattr(member, 'profile') and member.profile.notify_messages:
+                Notification.objects.create(
+                    recipient=member,
+                    sender=instance.sender,
+                    notification_type='message',
+                    message=f'{instance.sender.username} sent you a message',
+                    link=f'/chat/room/{instance.room.id}/'
+                )
